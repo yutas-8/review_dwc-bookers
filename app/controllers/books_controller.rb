a02377af244feac6ae6bbd3f05d3cca4ec6ef1fc
EC2 Default User <ec2-user@ip-172-31-59-143.ec2.internal>
2021-02-01 13:02:34 +0000

    first commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,22 @@
+class BooksController < ApplicationController
+  def top
+  end
+  
+  def index
+  end
+  
+  def show
+  end
+  
+  def create
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+end
